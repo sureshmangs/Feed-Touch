81b3fcd98640313f701b7f9fbce5b9bfe07b65f1
sureshmangs <sureshmangs1@gmail.com>
2020-04-30 16:21:36 +0530

    Adding routing in index.js and adding images

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
+import HomePage from './components/HomePage'
 import * as serviceWorker from './serviceWorker';
+const routing = (
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <Router>
+    <Switch>
+      <App>
+        <Route exact path="/" component={HomePage} />
+      </App>
+    </Switch>
+  </Router>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(routing, document.getElementById('root'));
+
 serviceWorker.unregister();
